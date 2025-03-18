0170dcf739d5e48c1162a78dfed40ec34303cab7
mr-ravin <mr.ravin_kumar@hotmail.com>
2025-03-18 17:35:53 +0530

    improvised screen text

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-This file contains FileChat-RAG program implemented using Python. 
+This file contains FileChat-RAG program implemented using Python.
 FileChat-RAG is a simple Retrieval-Augmented Generation (RAG) system that allows users to ask questions about the contents of various file formats.
 It extracts text from PDFs, JSON, text files, and code files, then enables interactive conversations using an LLM powered by Ollama.
 """
